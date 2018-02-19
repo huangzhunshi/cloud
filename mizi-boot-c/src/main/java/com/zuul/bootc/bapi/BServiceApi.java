0466583f710695aeec8d4b0f51f8d76b708eb2df
yihui <123>
2018-02-19 23:38:45 +0800

    添加注释

--- mizi-boot-c/src/main/java/com/zuul/bootc/bapi/BServiceApi.java
+++ mizi-boot-c/src/main/java/com/zuul/bootc/bapi/BServiceApi.java
@@ -3,6 +3,9 @@ package com.zuul.bootc.bapi;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * 远程服务调用
+ */
 @FeignClient(name = "service-hi-b",fallback = BServiceHystrix.class)
 public interface BServiceApi {
 
