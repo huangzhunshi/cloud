2420ee49a95a9f0c205c7469fb717196e2e62484
huangzhunshi <huangzhunshi@126.com>
2018-02-19 23:44:30 +0800

    Update BServiceApi.java

--- mizi-boot-c/src/main/java/com/zuul/bootc/bapi/BServiceApi.java
+++ mizi-boot-c/src/main/java/com/zuul/bootc/bapi/BServiceApi.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * 远程服务调用
+ * 远程微服务调动
  */
 @FeignClient(name = "service-hi-b",fallback = BServiceHystrix.class)
 public interface BServiceApi {
