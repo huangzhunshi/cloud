46e1dd50c39a074978b09cd26641eb0845f69e38
yihui <123>
2018-04-24 19:34:07 +0800

    提交相关项目demo

--- mizi-boot-c/src/main/java/com/zuul/bootc/RuleTest.java
+++ mizi-boot-c/src/main/java/com/zuul/bootc/RuleTest.java
@@ -1,11 +1,46 @@
 package com.zuul.bootc;
 
+import com.zuul.bootc.drools.model.Product;
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 
 public class RuleTest {
     public static void main(String[] arg){
+        //sample();
+        //my();
+        product();
+    }
+
+    private static void product(){
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kContainer = ks.getKieClasspathContainer();
+        KieSession kSession = kContainer.newKieSession("ksession-rules");
+
+        Product product=new Product();
+
+        product.setType(Product.DIAMOND);
+        kSession.insert(product);
+        kSession.fireAllRules();
+        kSession.dispose();
+
+        System.out.println("The discount for the product " + product.getType() + " is " + product.getDiscount()+"%");
+    }
+
+    private static void my(){
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kContainer = ks.getKieClasspathContainer();
+        KieSession kSession = kContainer.newKieSession("ksession-rules");
+
+ //       kSession.insert("haha");
+        Message message=new Message();
+        message.setStatus(0);
+        message.setMessage("haha");
+        kSession.insert(message);
+        kSession.fireAllRules();
+    }
+
+    private static void sample(){
         try {
             // load up the knowledge base
             KieServices ks = KieServices.Factory.get();
@@ -18,6 +53,12 @@ public class RuleTest {
             message.setMessage("GoodBye");
             message.setStatus(Message.HELLO);
             kSession.insert(message);
+
+            message.setMessage("Hello World");
+            message.setStatus(Message.HELLO);
+            kSession.insert(message);
+
+
             kSession.fireAllRules();
         } catch (Throwable t) {
             t.printStackTrace();
---
+++ mizi-boot-c/src/main/java/com/zuul/bootc/drools/model/Product.java
@@ -0,0 +1,38 @@
+package com.zuul.bootc.drools.model;
+
+/**
+ * 产品类
+ * Created by zhuzs on 2017/7/4.
+ */
+public class Product {
+
+    /**
+     * 钻石
+     */
+    public static final String DIAMOND = "0";
+
+    /**
+     * 黄金
+     */
+    public static final String GOLD = "1";
+
+    private String type;
+
+    private int discount;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(int discount) {
+        this.discount = discount;
+    }
+}
